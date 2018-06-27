899751a55eebe4effea2c6f35f6f6da9a28046fe
deusyvan <deusyvan@gmail.com>
2018-06-27 19:00:48 -0300

    Update index.php
    Recuperando a data time e transformando no padrão brasileiro.

--- index.php
+++ index.php
@@ -12,5 +12,7 @@
 $lista = $reservas->getReservas();
 
 foreach($lista as $item) {
+  $data1 = date('d/m/Y', strtotime($item['data_inicio']));
+  $data2 = date('d/m/Y', strtotime($item['data_fim']));
   echo $item['pessoa'].' reservou o carro '.$item['id_carro'].' entre '.$data1.' e '.$data2.'<br/>';
 }
