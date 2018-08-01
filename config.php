038a99a57e154bd96436f8f0442b350399e46d1a
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-08-01 11:25:44 -0300

    Atualização do projeto

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
   try {
-    $pdo = new PDO("mysql:dbname=projeto_reserva;host=localhost", "admin", "admin");
+    $pdo = new PDO("mysql:dbname=projeto_reservas;host=localhost", "admin", "admin@12");
   } catch(PDOException $e) {
     echo "ERRO: ".$e->getMessage();
     exit;
